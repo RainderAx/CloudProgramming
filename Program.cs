c5709faf92c6b0f554d19706a646860c21884fcf
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-11-05 20:14:46 +0100

    ca marche encore moins

--- Program.cs
+++ Program.cs
@@ -2,24 +2,18 @@
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
-
-
-
-
-
-
-collection.InsertOne(document);
+using System.Net.Http;
+using Newtonsoft.Json;
 
 class Program
 {
     static async Task Main(string[] args)
     {
         var client = new MongoClient("mongodb://localhost:27017");
-
-        var database = client.GetDatabase("TestMongoDB");
         IMongoDatabase database = client.GetDatabase("TraitementDeDonnee");
 
         string apiUrl = "https://explore.data.gouv.fr/?url=https%3A%2F%2Fwww.data.gouv.fr%2Ffr%2Fdatasets%2Fr%2Fe3d83ab3-dc52-4c99-abaf-8a38050cc68c";
+        HttpClient httpClient = new HttpClient();
         HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
 
         if (response.IsSuccessStatusCode)
