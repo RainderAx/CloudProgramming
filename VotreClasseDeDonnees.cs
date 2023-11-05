578c72685587f7dcc8c42e2f18974c48154ef44f
RainderAx <114318268+RainderAx@users.noreply.github.com>
2023-11-05 21:22:38 +0100

    votreclassededonnees

---
+++ VotreClasseDeDonnees.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+public class VotreClasseDeDonnees
+{
+	public VotreClasseDeDonnees()
+	{
+		public DateTime jour;
+		public int clage_90;
+		public int PourAvec;
+		public float tx_indic_7J_DC;
+		public float tx_indic_7J_hosp;
+		public float tx_indic_7J_SC;
+		public float tx_prev_hosp;
+		public float tx_prev_SC;
+	
+	}
+}
